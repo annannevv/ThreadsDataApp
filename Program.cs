129621f8e177b4e151f94efe78f26180e27e4d54
annannevv <annannevver@gmail.com>
2024-12-10 20:02:18 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using ConcurrentDataApp.Interfaces;
+using ConcurrentDataApp.Services;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IGenerator _generator = new Generator(new Random());
+        int numberOfTasks = 1000;
+        var phoneBook = new ConcurrentDictionary<string, string>();
+        var cancellationTokenSource = new System.Threading.CancellationTokenSource();
+
+        var tasks = Enumerable.Range(0, numberOfTasks).Select(Print).ToList();
+
+        var checkTask = Check();
+
+        await Task.Delay(5000);
+        cancellationTokenSource.Cancel();
+
+        tasks.Add(checkTask);
+        await Task.WhenAll(tasks);
+
+        async Task Print(int i)
+        {
+            string phoneNumber = await _generator.GenerateNumberAsync();
+            string name = await _generator.GenerateNameAsync();
+
+            Console.WriteLine($"{name} - {phoneNumber}");
+
+            phoneBook.TryAdd(phoneNumber, name);
+            await Task.Delay(1000);
+        }
+
+        async Task Check()
+        {
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                var results = phoneBook
+                        .Where(entry => entry.Value.Count(c => c == 'b') >= 2)
+                        .ToList();
+
+                foreach (var entry in results)
+                {
+                    Console.WriteLine($"Match found! Phone: {entry.Key}, Name: {entry.Value}");
+                }
+                await Task.Delay(500);
+            }
+        }
+    }
+}
+
---
+++ Services/Generator.cs
@@ -0,0 +1,48 @@
+﻿using ConcurrentDataApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentDataApp.Services
+{
+    internal class Generator : IGenerator
+    {
+        private static readonly string _letters = "abcdefghijklmnopqrstuvwxyz";
+        private static Random _random;
+
+        public Generator(Random random) 
+        { 
+            _random = random;
+        }
+        
+        public async Task<string> GenerateNameAsync()
+        {
+            return await Task.Run(() =>
+            {
+                var nameBuilder = new StringBuilder();
+                for (int i = 0; i < 5; i++)
+                {
+                    char letter = _letters[_random.Next(_letters.Length)];
+                    nameBuilder.Append(letter);
+                }
+                return nameBuilder.ToString();
+            });
+        }
+
+        public async Task<string> GenerateNumberAsync()
+        {
+            return await Task.Run(() =>
+            {
+                var phoneBuilder = new StringBuilder("+380");
+                for (int i = 0; i < 9; i++) 
+                {
+                    phoneBuilder.Append(_random.Next(0, 10));
+                }
+                return phoneBuilder.ToString();
+            });
+        }
+    }
+}
